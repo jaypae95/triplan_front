854d6fff15a70b3393d1fb2736a38df65835f165
JuHeon Ha <hjhj97@ajou.ac.kr>
2019-12-03 17:30:41 +0900

    pr again

--- src/router/index.js
+++ src/router/index.js
@@ -52,11 +52,11 @@ export default new Router({
       path: '/ConfirmPlan',
       name: 'ConfirmPlan',
       component: () => import('../views//ConfirmPlan.vue')
-    },
-    {
-      path: '/modaltest',
-      name: 'modaltest',
-      component: () => import('../views//modaltest.vue')
     }
+    // {
+    //   path: '/modaltest',
+    //   name: 'modaltest',
+    //   component: () => import('../views//modaltest.vue')
+    // }
   ]
 })
