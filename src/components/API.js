d8475feed2e58070e4b516f574a96f380007e301
JuHeon Ha <hjhj97@ajou.ac.kr>
2019-12-06 05:13:27 +0900

    add api, not finished

--- src/components/API.js
+++ src/components/API.js
@@ -14,8 +14,51 @@ function loginAPI ($http, host, data) {
   })
 }
 
+function registerAPI ($http, host, data) {
+  return $http({
+    method: 'post',
+    url: host + '/users/signup',
+    data
+  })
+}
+
+function checkingOverlap ($http, host, data) {
+  return $http({
+    method: 'post',
+    url: host + '/users/overlap',
+    data
+  })
+}
+
+function getCountryPositionAPI ($http, host, data) {
+  return $http({
+    method: 'post',
+    url: host + '/trip_plan/makeplan',
+    data
+  })
+}
+
+function getContinent ($http, host, name) {
+  return $http({
+    method: 'get',
+    url: host + `/trip_list/continent/${name}`
+  })
+}
+
+function getCountry ($http, host, id) {
+  return $http({
+    method: 'get',
+    url: host + `/trip_list/country/${id}`
+  })
+}
+
 // noinspection JSUnusedGlobalSymbols
 export default {
   httpTest,
-  loginAPI
+  loginAPI,
+  registerAPI,
+  checkingOverlap,
+  getCountryPositionAPI,
+  getContinent,
+  getCountry
 }
--- src/views/AttractionList.vue
+++ src/views/AttractionList.vue
@@ -1,6 +1,53 @@
 <template>
-<div>
+  <div>
     <p>Attraction List Page</p>
-     <router-link to="/">Go To Main</router-link><br>
-</div>
+    <router-link to="/">Go To Main</router-link><br><br>
+
+    <button v-on:click="selectContinent">Continent</button>
+    <button v-on:click="selectCountry">Country</button>
+
+    <!--  <select class="form-control" @change="selectContinent()">-->
+    <!--    <option value="" disabled>Select a continent</option>-->
+    <!--    <option v-for="conti in continents" v-bind:key="conti.id">{{conti}}</option>-->
+    <!--  </select>-->
+
+    <p>continents : {{continents[0]}}</p>
+    <p>countries : {{countries[0]}}</p>
+
+  </div>
 </template>
+
+<script>
+import API from '../components/API'
+
+export default {
+  data () {
+    return {
+      continents: [],
+      countries: [],
+      cities: []
+    }
+  },
+
+  methods: {
+    selectContinent () {
+      API.getContinent(this.$http, this.$env.apiUrl, 'asia').then(res => {
+        console.log(res)
+        this.continents.push(res.data)
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+
+    selectCountry () {
+      API.getCountry(this.$http, this.$env.apiUrl, '13').then(res => {
+        console.log(res)
+        this.countries.push(res.data)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
+
+}
+</script>
