988c49a9cf4f610fe05edd0ce7bda8da1f60feca
jaypae95 <jaypae95@hbsmith.io>
2019-12-07 00:04:26 +0900

    tmp

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,12 +1,58 @@
 <template>
   <div id="mypage">
-    <h1>This is My Page</h1>
+    <h1>마이 페이지</h1>
+    <table>
+      <tr>
+        <td>이름</td>
+        <td>{{user.user_name}}</td>
+      </tr>
+      <tr>
+        <td>아이디</td>
+        <td>{{user.user_id}}</td>
+      </tr>
+      <tr>
+        <td>이메일</td>
+        <td>{{user.user_email}}</td>
+      </tr>
+      <tr>
+        <td>성별</td>
+        <td v-if="user.user_gender">남</td>
+        <td v-else>여</td>
+      </tr>
+      <tr>
+        <td>연락처</td>
+        <td>{{user.user_phone}}</td>
+      </tr>
+    </table>
+    <button v-on:click="haha"></button>
     <router-link to='/'>Go To Main</router-link>
-
   </div>
 
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      user: this.$store.state.user
+      plan: ''
+    }
+  },
+  methods: {
+    haha () {
+      console.log(this.$store.state.user)
+    },
 
+  }
+}
 </script>
+<style>
+  table {
+    margin: auto;
+    text-align: center;
+  }
+
+  td {
+    padding: 10px;
+  }
+</style>
