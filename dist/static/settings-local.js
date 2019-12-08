1665a5fa548aea1df9b915c01c878808a783d956
jaypae95 <jaypae95@hbsmith.io>
2019-12-08 19:04:14 +0900

    add dist

---
+++ dist/static/settings-local.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+const apiUrl = 'http://15.164.224.135'
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    apiUrl: apiUrl
+  }
+}
