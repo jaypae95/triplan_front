87186d3ee564155718be5ff980fe8e59a678b640
jaypae95 <jaypae95@hbsmith.io>
2019-12-09 14:09:35 +0900

    days appear right away

--- src/views/MakePlan.vue
+++ src/views/MakePlan.vue
@@ -11,7 +11,7 @@
         </button>
       </div>
       <br><br>
-      <button id="addBtn" v-on:click="newAdd">+</button>
+      <button id="addBtn"></button>
 
     </div>
     <div>
@@ -20,10 +20,9 @@
           <p class="w1">관광지</p>
         </div>
         <br>
-        <button v-on:click="cl1" class="btn"><img id="illu1" src="../assets/choose.png"></button>
+        <button v-on:click="cl1" class="btn"><img class="illu1" src="../assets/choose.png"></button>
         <div v-if="showM" @close="showM=false">
           <select @change="selectCity($event)" name="city">
-            <!--      <option disabled value="">Select City</option>-->
             <option></option>
             <option v-for="city in cities" v-bind:key="city.id">{{city.city_name}}</option>
           </select>
@@ -44,8 +43,8 @@
           <div id="div_tour"> {{pl.place_name}}
           </div>
         </div>
-        <button class="btn" v-on:click="addTour"><img id="illu1" src="../assets/submit.png"></button>
-        <button class="btn" v-on:click="sendResult"><img id="illu2" src="../assets/totalSubmit.png"></button>
+        <button class="btn" v-on:click="addTour"><img class="illu1" src="../assets/submit.png"></button>
+        <button class="btn" v-on:click="sendResult"><img class="illu2" src="../assets/totalSubmit.png"></button>
       </div>
     </div>
   </div>
@@ -189,11 +188,14 @@ export default {
   },
   components: {
     mapping
+  },
+  created: function () {
+    this.newAdd()
   }
 }
 </script>
 <style>
-  #illu1, #illu2 {
+  .illu1, .illu2 {
     width: 80px;
     height: 35px;
   }
