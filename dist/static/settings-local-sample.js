8209cf663d73b980346eea9e95ef21ad57a7b36c
jaypae95 <jaypae95@hbsmith.io>
2019-12-12 10:18:27 +0900

    ignore dist

--- dist/static/settings-local-sample.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-const apiUrl = 'http://localhost:3000'
-
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-  module.exports = {
-    apiUrl: apiUrl
-  }
-}
